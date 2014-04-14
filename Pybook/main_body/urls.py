2149d7e09854bde5cb3b506de69cc7ed55629bb9
Alireza <a_marjani1994@yahoo.com>
2014-04-14 20:18:41 +0430

    search url

--- Pybook/main_body/urls.py
+++ Pybook/main_body/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     url(r'^(?P<user_name>.+)/wall/$', views.wall_view, name='wall'),
     url(r'^(?P<user_name>\w+)/profile/$',views.profile_view, name='profile'),
     url(r'^(?P<user_name>\W+)/(\d+)/$', views.topic_view, name='topic'),
+    url(r'^(?P<user_name>\W+)/(\d+)/$', views.search_view, name='search'),
     )
     
